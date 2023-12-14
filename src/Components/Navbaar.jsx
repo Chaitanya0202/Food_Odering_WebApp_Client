0c864fb4d2cfcd6bb006a4a26d62287de0e5398e
Chaitanya0202 <115619207+Chaitanya0202@users.noreply.github.com>
2023-12-14 13:04:16 +0530

    cartIcon Updated

--- src/Components/Navbaar.jsx
+++ src/Components/Navbaar.jsx
@@ -15,7 +15,7 @@ import MenuItem from "@mui/material/MenuItem";
 import Badge from "@mui/material/Badge";
 import { styled } from "@mui/material/styles";
 
-import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+
 
 const pages = ["Products", "Pricing", "Blog"];
 const settings = ["Profile", "Account", "Dashboard", "Logout"];
@@ -139,7 +139,7 @@ function Navbaar({ count }) {
           <Box sx={{ flexGrow: 0 }}>
             <IconButton aria-label="cart"  onClick={handleOpenUserMenu} >
               <StyledBadge badgeContent={count} color="secondary">
-                <ShoppingCartIcon />
+              <i className="material-icons">shopping_cart</i>
               </StyledBadge>
             </IconButton>
             <Tooltip title="Open settings">
